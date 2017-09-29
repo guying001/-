9fb4c3629f277ae2fcc3dd6855e2f6225178d209
PT-20170702IZJT\Administrator <1437968707@qq.com>
2017-09-29 16:35:52 +0800

    后台用户接口

--- application/admin/validate/User.php
+++ application/admin/validate/User.php
@@ -5,7 +5,7 @@
  * Date: 2017年8月30日
  * Time: 11:45
  */
-namespace app\api\validate;
+namespace app\admin\validate;
 
 use think\Validate;
 
@@ -25,7 +25,15 @@ class User extends Validate
         'openid' => 'require',
         'headimgurl' => 'require|url',
         'name' => 'require',
-        'sex' => 'in:1,2|number',
+        'sex' => 'in:1,2|number|require',
+        'email' => 'require',
+        'password' => 'require',
+        'repassword' => 'require',
+        'newpassword' => 'require',
+        'city' => 'require',
+        'id' => 'require',
+        'page' => 'require',
+        'limit' => 'require',
 
     ];
 
@@ -36,5 +44,23 @@ class User extends Validate
         'name.require'=>'请输入名称',
         'sex.in'=>'性别代码必须是1或者2',
         'sex.number'=>'性别代码必须是数字',
+        'sex.require'=>'请选择性别',
+        'email.require'=>'邮箱地址不能为空',
+        'password.require'=>'密码不能为空',
+        'repassword.require'=>'确认密码不能为空',
+        'newpassword.require'=>'新密码不能为空',
+        'city.require'=>'城市不能为空',
+        'id.require'=>'编号不能为空',
+        'page.require'=>'页码不能为空',
+        'limit.require'=>'条数不能为空',
+    ];
+
+    protected $scene = [
+        'user_add'   =>  ['email','name','password','repassword'],
+        'user_update'   =>  ['email','name','city','sex','id'],
+        'user_list'   =>  ['page','limit'],
+        'user_del'   =>  ['id'],
+        'user_dias'   =>  ['id'],
+        'user_updatePass'   =>  ['id','password','repassword','newpassword'],
     ];
 }
